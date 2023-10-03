ca2d0ea48a2f3ef699bf8140cff2ea2427916ee3
Pascal Thomet <pthomet@gmail.com>
2023-10-03 22:40:50 +0200

    + imgui_te_python_gil: handle GIL transfer between main and coro thread
    only used if IMGUI_TEST_ENGINE_WITH_PYTHON is defined

--- imgui_test_engine/imgui_te_engine.cpp
+++ imgui_test_engine/imgui_te_engine.cpp
@@ -33,6 +33,10 @@
 #pragma warning (disable: 4127) // conditional expression is constant
 #endif
 
+#ifdef IMGUI_TEST_ENGINE_WITH_PYTHON
+#include "imgui_te_python_gil.h"
+#endif
+
 /*
 
 Index of this file:
@@ -260,7 +264,13 @@ void    ImGuiTestEngine_Start(ImGuiTestEngine* engine, ImGuiContext* ui_ctx)
     if (!engine->TestQueueCoroutine)
     {
         IM_ASSERT(engine->IO.CoroutineFuncs && "Missing CoroutineFuncs! Use '#define IMGUI_TEST_ENGINE_ENABLE_COROUTINE_STDTHREAD_IMPL 1' or define your own implementation!");
-        engine->TestQueueCoroutine = engine->IO.CoroutineFuncs->CreateFunc(ImGuiTestEngine_TestQueueCoroutineMain, "Main Dear ImGui Test Thread", engine);
+        {
+            #ifdef IMGUI_TEST_ENGINE_WITH_PYTHON
+            // Release the GIL on the main thread, to enable it to be acquired temporarily on the new coroutine thread
+            PythonGIL::ReleaseGilOnMainThread_Scoped release;
+            #endif
+            engine->TestQueueCoroutine = engine->IO.CoroutineFuncs->CreateFunc(ImGuiTestEngine_TestQueueCoroutineMain, "Main Dear ImGui Test Thread", engine);
+        }
     }
     engine->Started = true;
 }
@@ -289,6 +299,10 @@ static void    ImGuiTestEngine_CoroutineStopAndJoin(ImGuiTestEngine* engine)
         engine->TestQueueCoroutineShouldExit = true;
         while (true)
         {
+            #ifdef IMGUI_TEST_ENGINE_WITH_PYTHON
+            // Release the GIL on the main thread, to enable it to be acquired temporarily on the new coroutine thread
+            PythonGIL::ReleaseGilOnMainThread_Scoped release;
+            #endif
             if (!engine->IO.CoroutineFuncs->RunFunc(engine->TestQueueCoroutine))
                 break;
         }
@@ -792,7 +806,14 @@ static void ImGuiTestEngine_PostNewFrame(ImGuiTestEngine* engine, ImGuiContext* 
     // or the loop in ImGuiTestEngine_TestQueueCoroutineMain that does so if no test is running.
     // If you want to breakpoint the point execution continues in the test code, breakpoint the exit condition in YieldFromCoroutine()
     const int input_queue_size_before = ui_ctx->InputEventsQueue.Size;
-    engine->IO.CoroutineFuncs->RunFunc(engine->TestQueueCoroutine);
+
+    {
+        #ifdef IMGUI_TEST_ENGINE_WITH_PYTHON
+        // Release the GIL on the main thread, to enable it to be acquired temporarily on the new coroutine thread
+        PythonGIL::ReleaseGilOnMainThread_Scoped release;
+        #endif
+        engine->IO.CoroutineFuncs->RunFunc(engine->TestQueueCoroutine);
+    }
 
     // Events added by TestFunc() marked automaticaly to not be deleted
     if (engine->TestContext && (engine->TestContext->RunFlags & ImGuiTestRunFlags_EnableRawInputs))
