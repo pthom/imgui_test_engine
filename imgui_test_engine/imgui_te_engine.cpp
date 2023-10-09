3f7b8e481d1cb64c5694854733b822bc14062119
Pascal Thomet <pthomet@gmail.com>
2023-10-09 13:17:29 +0200

    [Bundle] IMGUI_TEST_ENGINE_WITH_PYTHON => IMGUI_TEST_ENGINE_WITH_PYTHON_GIL

--- imgui_test_engine/imgui_te_engine.cpp
+++ imgui_test_engine/imgui_te_engine.cpp
@@ -45,7 +45,7 @@
 #pragma GCC diagnostic ignored "-Wsign-conversion"                  // warning: conversion to 'xxxx' from 'xxxx' may change the sign of the result
 #endif
 
-#ifdef IMGUI_TEST_ENGINE_WITH_PYTHON
+#ifdef IMGUI_TEST_ENGINE_WITH_PYTHON_GIL
 #include "imgui_te_python_gil.h"
 #endif
 
@@ -284,7 +284,7 @@ void    ImGuiTestEngine_Start(ImGuiTestEngine* engine, ImGuiContext* ui_ctx)
     {
         IM_ASSERT(engine->IO.CoroutineFuncs && "Missing CoroutineFuncs! Use '#define IMGUI_TEST_ENGINE_ENABLE_COROUTINE_STDTHREAD_IMPL 1' or define your own implementation!");
         {
-            #ifdef IMGUI_TEST_ENGINE_WITH_PYTHON
+            #ifdef IMGUI_TEST_ENGINE_WITH_PYTHON_GIL
             // Release the GIL on the main thread, to enable it to be acquired temporarily on the new coroutine thread
             PythonGIL::ReleaseGilOnMainThread_Scoped release;
             #endif
@@ -321,7 +321,7 @@ static void    ImGuiTestEngine_CoroutineStopAndJoin(ImGuiTestEngine* engine)
         engine->TestQueueCoroutineShouldExit = true;
         while (true)
         {
-            #ifdef IMGUI_TEST_ENGINE_WITH_PYTHON
+            #ifdef IMGUI_TEST_ENGINE_WITH_PYTHON_GIL
             // Release the GIL on the main thread, to enable it to be acquired temporarily on the new coroutine thread
             PythonGIL::ReleaseGilOnMainThread_Scoped release;
             #endif
@@ -952,7 +952,7 @@ static void ImGuiTestEngine_PreEndFrame(ImGuiTestEngine* engine, ImGuiContext* u
     // (process on-going queues in a coroutine)
 
     {
-        #ifdef IMGUI_TEST_ENGINE_WITH_PYTHON
+        #ifdef IMGUI_TEST_ENGINE_WITH_PYTHON_GIL
         // Release the GIL on the main thread, to enable it to be acquired temporarily on the new coroutine thread
         PythonGIL::ReleaseGilOnMainThread_Scoped release;
         #endif
