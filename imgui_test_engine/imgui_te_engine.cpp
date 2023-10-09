67b0a225938f55b095d47144c1b69dd87a4ad946
Pascal Thomet <pthomet@gmail.com>
2023-10-09 22:51:12 +0200

    [Bundle] simplify ImGuiTestEnginePythonGIL: only use pybind

--- imgui_test_engine/imgui_te_engine.cpp
+++ imgui_test_engine/imgui_te_engine.cpp
@@ -280,7 +280,7 @@ void    ImGuiTestEngine_Start(ImGuiTestEngine* engine, ImGuiContext* ui_ctx)
         {
             #ifdef IMGUI_TEST_ENGINE_WITH_PYTHON_GIL
             // Release the GIL on the main thread, to enable it to be acquired temporarily on the new coroutine thread
-            PythonGIL::ReleaseGilOnMainThread_Scoped release;
+            ImGuiTestEnginePythonGIL::ReleaseGilOnMainThread_Scoped release;
             #endif
             engine->TestQueueCoroutine = engine->IO.CoroutineFuncs->CreateFunc(ImGuiTestEngine_TestQueueCoroutineMain, "Main Dear ImGui Test Thread", engine);
         }
@@ -317,7 +317,7 @@ static void    ImGuiTestEngine_CoroutineStopAndJoin(ImGuiTestEngine* engine)
         {
             #ifdef IMGUI_TEST_ENGINE_WITH_PYTHON_GIL
             // Release the GIL on the main thread, to enable it to be acquired temporarily on the new coroutine thread
-            PythonGIL::ReleaseGilOnMainThread_Scoped release;
+            ImGuiTestEnginePythonGIL::ReleaseGilOnMainThread_Scoped release;
             #endif
             if (!engine->IO.CoroutineFuncs->RunFunc(engine->TestQueueCoroutine))
                 break;
@@ -925,7 +925,7 @@ static void ImGuiTestEngine_PreEndFrame(ImGuiTestEngine* engine, ImGuiContext* u
     {
         #ifdef IMGUI_TEST_ENGINE_WITH_PYTHON_GIL
         // Release the GIL on the main thread, to enable it to be acquired temporarily on the new coroutine thread
-        PythonGIL::ReleaseGilOnMainThread_Scoped release;
+        ImGuiTestEnginePythonGIL::ReleaseGilOnMainThread_Scoped release;
         #endif
         ImGuiTestEngine_RunTestFunc(engine);
     }
