c878019d751a092c0e3f5e694485885a1d3494ca
Pascal Thomet <pthomet@gmail.com>
2024-01-15 21:32:30 +0100

    [Bundle] imgui_test_engine / struct ImGuiTest: store Category, Name & SourceFile as Str

--- imgui_test_engine/imgui_te_engine.h
+++ imgui_test_engine/imgui_te_engine.h
@@ -30,6 +30,7 @@
 #endif
 
 #include "imgui_capture_tool.h"     // ImGuiScreenCaptureFunc
+#include "thirdparty/Str/Str.h"
 
 //-------------------------------------------------------------------------
 // Forward Declarations
@@ -393,11 +394,10 @@ struct IMGUI_API ImGuiTestOutput
 struct IMGUI_API ImGuiTest
 {
     // Test Definition
-    const char*                     Category = NULL;                // Literal, not owned
-    const char*                     Name = NULL;                    // Literal, generally not owned unless NameOwned=true
+    Str30                           Category;                       // Stored on the stack if len<30
+    Str30                           Name;                           // Stored on the stack if len<30
     ImGuiTestGroup                  Group = ImGuiTestGroup_Unknown; // Coarse groups: 'Tests' or 'Perf'
-    bool                            NameOwned = false;              //
-    const char*                     SourceFile = NULL;              // __FILE__
+    Str256                          SourceFile;                     // __FILE__, stored on the stack if len<256
     int                             SourceLine = 0;                 // __LINE__
     int                             SourceLineEnd = 0;              // Calculated by ImGuiTestEngine_StartCalcSourceLineEnds()
     int                             ArgVariant = 0;                 // User parameter. Generally we use it to run variations of a same test by sharing GuiFunc/TestFunc
@@ -423,8 +423,6 @@ struct IMGUI_API ImGuiTest
     // Functions
     ImGuiTest() {}
     ~ImGuiTest();
-
-    void SetOwnedName(const char* name);
 
     template <typename T>
     void SetVarsDataType(void(*post_initialize)(ImGuiTestContext* ctx, T& vars) = NULL)
