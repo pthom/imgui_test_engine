334795abec2d90746686e142428a4e9e0f0cbb79
Pascal Thomet <pthomet@gmail.com>
2024-01-18 11:30:49 +0100

    [Bundle] ImGuiTestEnginePythonGIL: can log

--- imgui_test_engine/imgui_te_python_gil.cpp
+++ imgui_test_engine/imgui_te_python_gil.cpp
@@ -7,6 +7,9 @@ namespace py = pybind11;
 
 #include <memory>
 
+//#define LOG_GIL(x) printf(x)
+#define LOG_GIL(x)
+
 
 namespace ImGuiTestEnginePythonGIL
 {
@@ -14,16 +17,32 @@ namespace ImGuiTestEnginePythonGIL
     ReleaseGilOnMainThread_Scoped::ReleaseGilOnMainThread_Scoped()
     {
         if (!Py_IsInitialized())
+        {
+            LOG_GIL("ReleaseGilOnMainThread_Scoped: Py_IsInitialized() == false\n");
             return;
+        }
+        LOG_GIL("ReleaseGilOnMainThread_Scoped: start...\n");
         _impl = static_cast<void *>(new py::gil_scoped_release());
+        LOG_GIL("ReleaseGilOnMainThread_Scoped: done...\n");
     }
 
     ReleaseGilOnMainThread_Scoped::~ReleaseGilOnMainThread_Scoped()
     {
         if (!Py_IsInitialized())
+        {
+            LOG_GIL("~ReleaseGilOnMainThread_Scoped: Py_IsInitialized() == false\n");
             return;
+        }
         if (_impl)
+        {
+            LOG_GIL("~ReleaseGilOnMainThread_Scoped: start...\n");
             delete static_cast<py::gil_scoped_release *>(_impl);
+            LOG_GIL("~ReleaseGilOnMainThread_Scoped: done...\n");
+        }
+        else
+        {
+            LOG_GIL("~ReleaseGilOnMainThread_Scoped: _impl == nullptr\n");
+        }
     }
 
 
@@ -32,17 +51,27 @@ namespace ImGuiTestEnginePythonGIL
     void AcquireGilOnCoroThread()
     {
         if (!Py_IsInitialized())
+        {
+            LOG_GIL("AcquireGilOnCoroThread: Py_IsInitialized() == false\n");
             return;
+        }
         assert(GGilScopedAcquire.get() == nullptr);
+        LOG_GIL("AcquireGilOnCoroThread: start...\n");
         GGilScopedAcquire = std::make_unique<py::gil_scoped_acquire>();
+        LOG_GIL("AcquireGilOnCoroThread: done...\n");
     }
 
     void ReleaseGilOnCoroThread()
     {
         if (!Py_IsInitialized())
+        {
+            LOG_GIL("ReleaseGilOnCoroThread: Py_IsInitialized() == false\n");
             return;
+        }
+        LOG_GIL("ReleaseGilOnCoroThread: start...\n");
         assert(GGilScopedAcquire.get() != nullptr);
         GGilScopedAcquire.reset();
+        LOG_GIL("ReleaseGilOnCoroThread: done...\n");
     }
 
 }
